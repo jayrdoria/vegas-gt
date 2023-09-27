b5566f6dd3095d7c520c52fafa6ede9121e82bd9
Jay-R Doria <jayrdoria876@gmail.com>
2023-09-27 13:56:52 +0800

    TopCasino, not done in tablet responsiveness

---
+++ vegas-gt/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import VegasGT from "../src/components/VegasGT";
+import TopCasino from "./components/TopCasino";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <VegasGT />
+      <TopCasino />
+    </div>
+  );
+}
+
+export default App;
---
+++ vegas-gt/src/components/VegasGT.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/style.css";
+
+const VegasGT = () => {
+  return <div className="vegas-gt-container"></div>;
+};
+
+export default VegasGT;
